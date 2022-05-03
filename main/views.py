a5412852d3c7342b5bbbed91c3e3c0910fa185fa
tionbec <78805548+tionbec@users.noreply.github.com>
2022-05-03 11:07:46 +0200

    Done after video 8

--- main/views.py
+++ main/views.py
@@ -6,6 +6,25 @@ from .forms import CreateNewList
 
 def index(response, id):
     ls = ToDoList.objects.get(id=id)
+    {"save":["save"], "c1":["clicked"]}
+    if response.method == "POST":
+        print(response.POST)
+        if response.POST.get("save"):
+            for item in ls.item_set.all():
+                if response.POST.get("c" + str(item.id)) == "clicked":
+                    item.complete = True
+                else:
+                    item.complete = False
+
+                item.save()
+        elif response.POST.get("newItem"):
+            txt = response.POST.get("new")
+
+            if len(txt) > 2:
+                ls.item_set.create(text=txt, complete=False)
+            else:
+                print("invalid")
+
     return render(response, "main/list.html", {"ls": ls})
 
 def home(response):
@@ -14,13 +33,11 @@ def home(response):
 def create(response):
     if response.method == "POST":
         form = CreateNewList(response.POST)
-
         if form.is_valid():
             n = form.cleaned_data["name"]
             t = ToDoList(name=n)
             t.save()
-
-        return HttpResponseRedirect("/%i" %t.id)
-    else :
+            return HttpResponseRedirect("/%i" %t.id)
+    else:
         form = CreateNewList()
     return render(response, "main/create.html", {"form":form})
